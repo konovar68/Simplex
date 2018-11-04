a37897767f7f0ba2d1ab3307241bcd85ca39125a
Артем Коновской <konowscoi.artem@hotmail.com>
2018-11-04 13:18:12 +0300

    Download

---
+++ SIMPLEX/SIMPLEX/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SIMPLEX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.helpProvider1.SetHelpString(this.dataGridView1, "Если вы, хотите ввести строку \"3х+2y<=5\", то в матрицу вы должны внести 5 3 2");
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.helpProvider1.SetShowHelp(this.dataGridView1, true);
+            this.dataGridView1.Size = new System.Drawing.Size(400, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Строки";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(422, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(500, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(43, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(497, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Столбцы";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(578, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 406);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(400, 400);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(442, 406);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(121, 400);
+            this.dataGridView3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Image = global::SIMPLEX.Properties.Resources._579219815fe9d15612b3a1712;
+            this.button1.Location = new System.Drawing.Point(422, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 242);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(659, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(13, 14);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(686, 812);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.HelpButton = true;
+            this.helpProvider1.SetHelpNavigator(this, System.Windows.Forms.HelpNavigator.Topic);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.helpProvider1.SetShowHelp(this, true);
+            this.Text = "i did it for the lulz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SIMPLEX/SIMPLEX/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIMPLEX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int s1, s2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+        }
+        void datagrid()
+        {
+            dataGridView1.Columns.Clear();
+            s1 = int.Parse(textBox1.Text);
+            s2 = int.Parse(textBox2.Text);
+            for (int i = 0; i < s2; i++)
+            {
+                dataGridView1.Columns.Add("column1", "");
+                dataGridView1.Columns[i].Width = 30;
+            }
+            for (int i = 0; i < s1; i++)
+                dataGridView1.Rows.Add();
+            
+        }
+        public class Simplex
+        {
+            //source - симплекс таблица без базисных переменных
+            double[,] table; //симплекс таблица
+
+            int m, n;
+
+            List<int> basis; //список базисных переменных
+
+            public Simplex(double[,] source)
+            {
+                m = source.GetLength(0);
+                n = source.GetLength(1);
+                table = new double[m, n + m - 1];
+                basis = new List<int>();
+
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < table.GetLength(1); j++)
+                    {
+                        if (j < n)
+                            table[i, j] = source[i, j];
+                        else
+                            table[i, j] = 0;
+                    }
+                    //выставляем коэффициент 1 перед базисной переменной в строке
+                    if ((n + i) < table.GetLength(1))
+                    {
+                        table[i, n + i] = 1;
+                        basis.Add(n + i);
+                    }
+                }
+
+                n = table.GetLength(1);
+            }
+
+            //result - в этот массив будут записаны полученные значения X
+            public double[,] Calculate(double[] result)
+            {
+                int mainCol, mainRow; //ведущие столбец и строка
+
+                while (!IsItEnd())
+                {
+                    mainCol = findMainCol();
+                    mainRow = findMainRow(mainCol);
+                    basis[mainRow] = mainCol;
+
+                    double[,] new_table = new double[m, n];
+
+                    for (int j = 0; j < n; j++)
+                        new_table[mainRow, j] = table[mainRow, j] / table[mainRow, mainCol];
+
+                    for (int i = 0; i < m; i++)
+                    {
+                        if (i == mainRow)
+                            continue;
+
+                        for (int j = 0; j < n; j++)
+                            new_table[i, j] = table[i, j] - table[i, mainCol] * new_table[mainRow, j];
+                    }
+                    table = new_table;
+                }
+
+                //заносим в result найденные значения X
+                for (int i = 0; i < result.Length; i++)
+                {
+                    int k = basis.IndexOf(i + 1);
+                    if (k != -1)
+                        result[i] = table[k, 0];
+                    else
+                        result[i] = 0;
+                }
+
+                return table;
+            }
+
+            private bool IsItEnd()
+            {
+                bool flag = true;
+
+                for (int j = 1; j < n; j++)
+                {
+                    if (table[m - 1, j] < 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                return flag;
+            }
+
+            private int findMainCol()
+            {
+                int mainCol = 1;
+
+                for (int j = 2; j < n; j++)
+                    if (table[m - 1, j] < table[m - 1, mainCol])
+                        mainCol = j;
+
+                return mainCol;
+            }
+
+            private int findMainRow(int mainCol)
+            {
+                int mainRow = 0;
+
+                for (int i = 0; i < m - 1; i++)
+                    if (table[i, mainCol] > 0)
+                    {
+                        mainRow = i;
+                        break;
+                    }
+
+                for (int i = mainRow + 1; i < m - 1; i++)
+                    if ((table[i, mainCol] > 0) && ((table[i, 0] / table[i, mainCol]) < (table[mainRow, 0] / table[mainRow, mainCol])))
+                        mainRow = i;
+
+                return mainRow;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            datagrid();
+            button1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text= 4.ToString();
+            s2 = s1 = 4;
+
+            button1.Visible = true;
+
+            dataGridView1.Columns.Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                dataGridView1.Columns.Add("column1", "");
+                dataGridView1.Columns[i].Width = 30;
+            }
+            for (int j = 0; j < 4; j++)
+                dataGridView1.Rows.Add();
+            //double[,] table = { {30, 1,  1, 3}, //x1+x2+3х3<=30
+            //                    {24, 2, 2, 5},//2x1+2x2+5х3<=24
+            //                    {36,  4, 1, 2},//4x1+х2+2х3<=36
+            //                    { 0, -3, -1, -2} }; //z= 3x1 + x2+2х3->max
+
+            dataGridView1[0, 0].Value = 30;
+            dataGridView1[1, 0].Value = 1;
+            dataGridView1[2, 0].Value = 1;
+            dataGridView1[3, 0].Value = 3;
+
+            dataGridView1[0, 1].Value = 24;
+            dataGridView1[1, 1].Value = 2;
+            dataGridView1[2, 1].Value = 2;
+            dataGridView1[3, 1].Value = 5;
+
+            dataGridView1[0, 2].Value = 36;
+            dataGridView1[1, 2].Value = 4;
+            dataGridView1[2, 2].Value = 1;
+            dataGridView1[3, 2].Value = 2;
+
+            dataGridView1[0, 3].Value = 0;
+            dataGridView1[1, 3].Value = -3;
+            dataGridView1[2, 3].Value = -1;
+            dataGridView1[3, 3].Value = -2;
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Columns.Clear();
+
+            dataGridView3.Columns.Clear();
+            dataGridView3.Columns.Add("column1", "");
+            dataGridView2.ReadOnly = true;
+            dataGridView3.ReadOnly = true;
+            dataGridView3.Columns[0].Width = 30;
+
+            double[,] table = new double[s1, s2];
+            for (int i = 0; i < s1; i++)
+                for (int j = 0; j < s2; j++)
+                    table[i, j] = Convert.ToDouble(dataGridView1[j, i].Value);
+
+            double[] result = new double[s2 - 1];                    
+
+            double[,] table_result;
+            Simplex S = new Simplex(table);
+            table_result = S.Calculate(result);
+
+            for (int i = 0; i < table_result.GetLength(1); i++)
+            {
+                dataGridView2.Columns.Add("column1", "");
+                dataGridView2.Columns[i].Width = 30;
+            }
+            for (int j = 0; j < table_result.GetLength(0); j++)
+                dataGridView2.Rows.Add();
+            
+            for (int i = 0; i < table_result.GetLength(0); i++)
+            {
+                for (int j = 0; j < table_result.GetLength(1); j++)
+                    dataGridView2[j, i].Value = table_result[i, j];                    
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                dataGridView3.Rows.Add();
+                dataGridView3[0, i].Value = result[i];
+            }
+        }
+    }
+}
